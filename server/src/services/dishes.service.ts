1c4d85e53e2e633b24b5172e26756419f22ea4c3
Jad <j.hamwi@centroidsol.com>
2023-05-28 15:00:42 +0300

    update

--- server/src/services/dishes.service.ts
+++ server/src/services/dishes.service.ts
@@ -144,53 +144,25 @@ const getDishes = async () => {
 };
 
 const updateDish = async (dishName: string, dish: IDish) => {
-	const ingredientsRepo = AppDataSource.getRepository(Ingredient);
 	const dishesRepo = AppDataSource.getRepository(Dish);
-	const dishesIngredientsRepo = AppDataSource.getRepository(DishIngredient);
-	const { ingredients } = dish;
+
 	const dishRecord = await dishesRepo.findOneBy({ name: dishName });
 	if (!dishRecord) {
 		throw new NotFoundError("dish to update : not found");
 	}
-	const dishesIngredientsToSave: DishIngredient[] = [];
-	dishRecord.dishIngredients = [];
 
-	for (const ingredient of ingredients) {
-		const ingredientRecord = await ingredientsRepo.findOneBy({
-			name: ingredient.name,
-		});
-		if (!ingredientRecord) {
-			await dishesRepo.remove(dishRecord);
-			throw new NotFoundError(`ingredient ${ingredient} not found`);
-		}
-
-		const dishIngredient = await dishesIngredientsRepo.findOneBy({
-			dish: dishRecord,
-		});
-
-		dishIngredient.dish = dishRecord;
-		dishIngredient.ingredient = ingredientRecord;
-		dishIngredient.amount = ingredient.amount;
-		dishesIngredientsToSave.push(dishIngredient);
-		dishRecord.dishIngredients.push(dishIngredient);
-	}
 	dishRecord.name = dish.name;
 	dishRecord.price = dish.price;
-	dishRecord.description = dish.description;
-
-	await dishesIngredientsRepo.save(dishesIngredientsToSave);
 	await dishesRepo.save(dishRecord);
 
+	const prevRedisDish = JSON.parse(
+		await RedisService.redis.hget("dishes", String(dishRecord.id))
+	);
+
 	const redisDish = {
+		...prevRedisDish,
 		name: dishRecord.name,
 		price: dishRecord.price,
-		description: dishRecord.description,
-		ingredients: dishRecord.dishIngredients.map((ingredient) => ({
-			name: ingredient.ingredient.name,
-			amount: ingredient.amount,
-			unit: ingredient.ingredient.unit,
-		})),
-		category: dishRecord.category.name,
 	};
 	await RedisService.redis.hset(
 		"dishes",
