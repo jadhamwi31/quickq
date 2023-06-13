37d4a7aef42446d79d82bd51c255c1f230cb20ee
Ahmad Abu-Alzahab <60927533+AhmadAbuAlzahab@users.noreply.github.com>
2023-06-13 08:49:07 -0800

    Delete frontend directory

--- frontend/src/pages/admin/Accounting.tsx
+++
@@ -1,9 +0,0 @@
-import { useEffect } from 'react'
-export default function Accounting() {
-    useEffect(() => {
-        document.title = `Admin | Accounting`;
-    }, []);
-    return (
-        <div>Accounting</div>
-    )
-}
